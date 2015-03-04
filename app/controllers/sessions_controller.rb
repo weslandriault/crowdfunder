1410b6c10675ef33c44f6a537658068300f7ea72
Wes <wesley.landriault@gmail.com>
2015-03-04 17:08:22 -0500

    Made changes to sessions controller and views adding more functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-    user = User.find_by(:email, params[:email])
-    if user && authenticate(params[:password])
-      session[:user_id] = user.user_id
+    @user = login(params[:email], params[:password])
+
+    if @user
+      session[:user_id] = @user.id
       redirect_to projects_url, notice: "Logged in"
     else
+      flash.now[:alert] = "Invalid email or password!"
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to projects_url, notice: "Logged out!"
   end
 end
