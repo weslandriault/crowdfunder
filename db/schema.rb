7be3d0d3dfb77fc048ef6452dd8ddd17118cc285
jonnylam <jonathanryanlam@gmail.com>
2015-03-05 18:31:22 -0500

    added remember me token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303201610) do
+ActiveRecord::Schema.define(version: 20150305225116) do
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
@@ -44,15 +44,18 @@ ActiveRecord::Schema.define(version: 20150303201610) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",            null: false
+    t.string   "email",                        null: false
     t.string   "crypted_password"
     t.string   "salt"
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
 
 end
