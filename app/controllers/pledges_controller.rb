0b3077f79b15cbf7bc1e0ea59ec336d7f7a01833
Wes <wesley.landriault@gmail.com>
2015-03-05 15:34:15 -0500

    Made changes to pledges controller, and projects views to include pledge functionality for each project.

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,31 @@
 class PledgesController < ApplicationController
+  before_action :load_project
+
+  def new
+    @pledge = @project.pledge.new
+  end
+
+  def create
+    @pledge = @project.pledge.new
+
+    if @pledge.save
+      redirect_to project_path(@project)
+    else
+      flash.now[:alert] = "Your attempt to make a pledge didn't work. Please try again!"
+    end
+  end
+
+  def destroy
+    @pledge = Pledge.find(params[:id])
+    @reward.destroy
+    redirect_to project_path(@project)
+  end
+
+  private
+  def pledge_params
+    params[:pledge].permit(:amount, :backer_id, :project_id)
+  end
+  def load_project
+    @project = Project.find(params[:project])
+  end
 end
