4f98b2109d9d01a1e69e754ba67e154984c5334a
jonnylam <jonathanryanlam@gmail.com>
2015-03-05 17:08:27 -0500

    Added action mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.welcome_email(@user).deliver_now
       redirect_to projects_url, notice: "Signed up!"
     else
       render "new"
