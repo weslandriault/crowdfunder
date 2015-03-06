c6856669b1a23a69db7d7becdce42a4649caf32f
jonnylam <jonathanryanlam@gmail.com>
2015-03-05 19:39:18 -0500

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20150305220431) do
 
   create_table "pledges", force: :cascade do |t|
@@ -51,8 +52,10 @@ ActiveRecord::Schema.define(version: 20150305220431) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token"
 end
